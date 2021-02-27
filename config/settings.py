5fae906efdcf9823dc07a1302575ae6539d8453c
Yash1599 <yashsinghania15@gmail.com>
2021-02-27 19:05:55 +0530

    Task6

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
